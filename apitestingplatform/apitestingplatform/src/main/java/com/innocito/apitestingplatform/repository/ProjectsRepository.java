64d969a78f71b873ef143198fb5c6d8f8b90cc67
Adapachaitanya <adapa7198@gmail.com>
2025-06-25 17:30:28 +0530

    Update ProjectsRepository.java

--- apitestingplatform/apitestingplatform/src/main/java/com/innocito/apitestingplatform/repository/ProjectsRepository.java
+++ apitestingplatform/apitestingplatform/src/main/java/com/innocito/apitestingplatform/repository/ProjectsRepository.java
@@ -1,12 +1,21 @@
 package com.innocito.apitestingplatform.repository;
 
+import com.innocito.apitestingplatform.apiresponse.ApiResponseMessage;
 import com.innocito.apitestingplatform.entity.ProjectsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
-public interface ProjectsRepository extends JpaRepository<ProjectsEntity, Long> {
-
+public interface ProjectsRepository extends JpaRepository<ProjectsEntity, Long>
+{
+   Optional<ProjectsEntity> findByProjectName(String projectName);
+   List<ProjectsEntity> findByCreatedByContainingIgnoreCase(String createdBy);
+   List<ProjectsEntity> findByCreationDate(LocalDate creationDate);
 
 }
